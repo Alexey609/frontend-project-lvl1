333e44a3c8093ce7b10864769efeefff68e25e43
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2019-09-13 17:59:44 +0500

    Update brain-progression.js

--- src/bin/brain-progression.js
+++ src/bin/brain-progression.js
@@ -1,9 +1,4 @@
-#!/usr/bin/env node
+import  progression from '../games/progression';
 
-import { welcome, name } from '..';
-import progression from '../games/progression';
-
-welcome();
-name();
 progression();
 console.log('');
