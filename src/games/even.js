dc85692e63ea4c1bb526b3ed95634eb2f352ba03
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2019-10-22 16:01:07 +0500

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,34 +1,21 @@
-import readlineSync from 'readline-sync';
-import { name } from '../index.js';
+import engine from '..';
+import randomNumber from './randomnumber';
 
 const even = () => {
-  console.log('Answer "yes" if number even otherwise answer "no".');
-  console.log('');
-  const count = (n) => {
-    if (n === 3) {
-      console.log(`Congratulations, ${name}!`);
-      return true;
-    }
-    const num = Math.round(100 * Math.random());
-    console.log(`Question: ${num}`);
-    const answer = readlineSync.question('Your answer: ');
-    if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer === 'no')) {
-      console.log('Correct!');
-      return n + count(n + 1);
-    }
-    if (num % 2 === 0 && answer !== 'yes') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${name}!`);
-      return false;
-    }
-    if (num % 2 !== 0 && answer !== 'no') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
-      console.log(`Let's try again, ${name}!`);
-      return false;
-    }
-    return true;
+  const gameDesciption = () => {
+    console.log('Answer "yes" if number even otherwise answer "no".\n');
   };
-  count(0);
+
+  const getData = () => {
+    const num = randomNumber();
+    if (num % 2 === 0) {
+      console.log(`Question: ${num}`);
+      return 'yes';
+    }
+    console.log(`Question: ${num}`);
+    return 'no';
+  };
+  engine(gameDesciption, getData);
 };
 
 export default even;
