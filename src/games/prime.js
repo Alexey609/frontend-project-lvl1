c84859f011bc60b0434e3514695ea1d25f69932a
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2020-02-07 13:44:48 +0500

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,10 +2,8 @@ import engine from '..';
 import randomNumber from './randomnumber';
 
 const prime = () => {
-  const gameDesciption = () => {
-    console.log('Answer "yes" if given number is prime. Otherwise answer "no".\n');
-  };
-
+  const gameDesciption = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
+  
   const question = (a) => {
     for (let i = 2; i < a; i += 1) {
       if (a % i === 0) {
