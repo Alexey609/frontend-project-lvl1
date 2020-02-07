724b8f4b653ef8cbcb5d38f5a08c2744514ca282
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2020-02-07 13:40:56 +0500

    Update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,10 +2,8 @@ import engine from '..';
 import randomNumber from './randomnumber';
 
 const calc = () => {
-  const gameDesciption = () => {
-    console.log('What is the result of the expression?\n');
-  };
-
+  const gameDesciption = 'What is the result of the expression?\n';
+  
   const getData = () => {
     const num1 = randomNumber();
     const num2 = randomNumber();
