c2e7336ed3ab8779ee65087b7a0e58ccb31bc4d2
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2020-02-07 20:53:24 +0500

    Update checknum.js

--- src/games/checknum.js
+++ src/games/checknum.js
@@ -1,9 +1,10 @@
 const checkNum = (num) => {
   if (num % 2 === 0) {
     return true;
-   } else {
-     return false;
-   }
+  } else {
+    return false;
+   } 
+  }
 };
 
 export default checkNum;
