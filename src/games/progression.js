7e2f55f4c3a70fab17ffc4a30c801f241f88fce0
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2020-03-03 14:04:59 +0500

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,56 +1,28 @@
-import engine from '..';
+import { generateRandomNumber } from '../utils';
+import makeGame from '..';
 
-const progression = () => {
-  const gameDesciption = 'What number is missing in the progression?\n';
-  const makeProgression = () => {
-    const step = 3;
-    const iter = (i, acc) => {
-      if (i === 10) {
-        return acc;
-      }
-      return iter(i + 1, `${acc} ${13 + step * i}`);
-    };
-    return iter(1, 13);
-  };
+const rule = 'What number is missing in the progression?';
 
-  const getData = () => {
-    const getProgression = makeProgression();
-    const randomIndex = (min = 0, max = (getProgression.length - 1)) => Math.floor(Math.random()
-    * (max - min)) + min;
+const getProgression = (start, step, length, hiddenValueIndex) => {
+  let result = '';
+  for (let i = 0; i < length; i += 1) {
+    result = i === hiddenValueIndex ? `${result} ..` : `${result} ${start + step * i}`;
+  }
 
-    const numIndex = (i) => {
-      if (getProgression[i] === ' ') {
-        return i - 2;
-      }
-      if (getProgression[i] !== ' ' && getProgression[i + 1] === ' ') {
-        return i - 1;
-      }
-      if (getProgression[i] !== ' ' && getProgression[i - 1] === ' ') {
-        return i;
-      }
-      if (i === 0) {
-        return i;
-      }
-      if (i === 28) {
-        return 27;
-      }
-      return true;
-    };
-
-    const index = randomIndex();
-
-    const correctAnswer = getProgression.slice(numIndex(index), numIndex(index) + 2);
-
-    const getQuestionProgression = (str) => {
-      if (numIndex(index) !== 0) {
-        return `${str.substring(0, numIndex(index))}..${str.substring(numIndex(index) + 2, str.length)}`;
-      }
-      return `..${str.substring(numIndex(index) + 2, str.length)}`;
-    };
-    console.log(`Question: ${getQuestionProgression(getProgression)}`);
-    return String(correctAnswer);
-  };
-  engine(gameDesciption, getData);
+  return result.trim();
 };
 
-export default progression;
+const progressLength = 10;
+const lastIndex = progressLength - 1;
+
+const makeRound = () => {
+  const hiddenValueIndex = generateRandomNumber(0, lastIndex);
+  const start = generateRandomNumber(1, 100);
+  const step = generateRandomNumber(1, 30);
+  const question = getProgression(start, step, progressLength, hiddenValueIndex);
+  const answer = (start + step * hiddenValueIndex).toString();
+
+  return { question, answer };
+};
+
+export default () => makeGame(rule, makeRound);
