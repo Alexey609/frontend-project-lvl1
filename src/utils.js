8020b561e4998751d9363788ddfaadb1ada22e7c
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2020-03-10 17:19:13 +0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,9 @@ const generateRandomNumber = (min, max) => Math.floor(min + Math.random() * (max
 
 const getQuestion = (data) => data.question;
 const getAnswer = (data) => data.answer;
-  
+
 export {
   generateRandomNumber,
   getQuestion,
-  getAnswer
+  getAnswer,
 };
