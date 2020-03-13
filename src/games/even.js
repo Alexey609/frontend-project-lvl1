f390a432c0b34f8010ffe42d3187d6822edaa06d
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2020-03-13 11:34:00 +0500

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,6 @@
 import { generateRandomNumber } from '../utils';
 import playGame from '..';
+import { makeData } from '../utils';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
