ae229d7ed8453904c743f85bf093220daab37057
Alexey609 <46045494+Alexey609@users.noreply.github.com>
2020-03-14 13:53:51 +0500

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -6,10 +6,9 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (value) => value % 2 === 0;
 
 const generateRound = () => {
-  makeData();
   const question = generateRandomNumber(1, 100);
   const answer = isEven(question) ? 'yes' : 'no';
-  return { question, answer };
+  return makeData (question, answer);
 };
 
 export default () => playGame(rule, generateRound);
